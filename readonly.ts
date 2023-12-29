823eee70bb41d08bd8939cd485efd592f2e61297
machamp0714 <machamp0714@gmail.com>
2023-12-29 12:06:02 +0900

    resolve: 7-readonly

---
+++ readonly.ts
@@ -0,0 +1,19 @@
+//github.com/type-challenges/type-challenges/blob/main/questions/00007-easy-readonly/README.ja.md
+
+https: interface Todo1 {
+  title: string;
+  description: string;
+}
+
+// 自分の回答
+type MyReadonly<T> = {
+  readonly [K in keyof T]: T[K];
+};
+
+const readonly: MyReadonly<Todo1> = {
+  title: "Hey",
+  description: "foobar",
+};
+
+// readonly.title = "Hello"; Error: cannot reassign a readonly property
+// readonly.description = "barFoo"; Error: cannot reassign a readonly property
